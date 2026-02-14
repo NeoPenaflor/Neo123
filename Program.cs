f78828adf66fa4ab3af4bf08730524574b0b542f
burger-ui <sanchezjed12@gmail.com>
2026-02-14 17:57:17 +0800

    yes

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace accountmanage
+{
+internal class Program
+{
+static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            string user = "Neo";
+            string pass = "Neo12345";
+
+            Console.WriteLine("Enter your username: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Enter your password: ");
+            string passInput = Console.ReadLine();
+
+            if (userInput == user && passInput == pass)
+            {
+            Console.WriteLine("Login Succesful Congrats!");
+            }
+            else
+            {
+            Console.WriteLine("Wrong credentials");
+            }
+        }
+    }
+}
